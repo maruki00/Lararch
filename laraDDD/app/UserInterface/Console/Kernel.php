53af472d50048c76fa9149cd640d80798e2ed958
Oulahyane Abdellah <aitbaha48@gmail.com>
2024-01-12 07:24:46 +0300

    laraDDD

--- laraDDD/app/UserInterface/Console/Kernel.php
+++ laraDDD/app/UserInterface/Console/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Infrastructure\Laravel\Console;
+namespace App\UserInterface\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
