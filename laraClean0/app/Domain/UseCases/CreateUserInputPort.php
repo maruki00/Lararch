4a6bb091588869c0cc3562f48434cc6f87f96b0b
Oulahyane Abdellah <aitbaha48@gmail.com>
2024-01-27 14:28:07 +0300

    --

--- laraClean0/app/Domain/UseCases/CreateUserInputPort.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Domain\UseCases;
-
-use App\Domain\Interfaces\ViewModel;
-
-interface CreateUserInputPort {
-    public function createUser(CreateUserResponseModel $model):ViewModel;
-}
